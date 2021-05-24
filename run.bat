ff8635b79ae1af640d0b2b0fef6c55ce4ac6357c
apodgornyitba <apodgorny@itba.edu.ar>
2021-05-24 15:13:42 -0300

    Funcionalidades de gdb agregadas

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-qemu-system-x86_64.exe -hda Image/x64BareBonesImage.qcow2
+@REM if [[ "$1" = "gdb" ]]
+@REM     qemu-system-x86_64.exe -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+@REM else
+@REM     qemu-system-x86_64.exe -hda Image/x64BareBonesImage.qcow2 -m 512
+qemu-system-x86_64.exe -hda Image/x64BareBonesImage.qcow2 -m 512
